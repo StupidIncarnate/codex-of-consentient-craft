aa3e1ec80b14b3450dfe5a1f0aecb0656bc51755
J Brutus <brutus@nighthorse.ai>
2025-07-04 20:17:00 -0700

    publish assets

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    files: ['tests/**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prefer-const': 'error',
+      'no-console': 'off'
+    }
+  }
+];
