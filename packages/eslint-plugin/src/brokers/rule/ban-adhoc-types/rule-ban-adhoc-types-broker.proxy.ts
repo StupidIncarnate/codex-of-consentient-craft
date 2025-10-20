a4565b735142f79a6f5c171f8013edb90331ff2a
J Brutus <StupidIncarnate@users.noreply.github.com>
2025-10-19 21:28:32 -0700

    Added missing proxy file

---
+++ packages/eslint-plugin/src/brokers/rule/ban-adhoc-types/rule-ban-adhoc-types-broker.proxy.ts
@@ -0,0 +1,5 @@
+/**
+ * Proxy for ban-adhoc-types rule broker.
+ * Empty proxy - ESLint rules run with real parsing to validate DSL logic.
+ */
+export const ruleBanAdhocTypesBrokerProxy = (): Record<PropertyKey, never> => ({});
